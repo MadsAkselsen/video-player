8905a1f213a6c08c93b318c0e54c1d75808743b8
Mads Akselsen <akselsenmads@gmail.com>
2021-01-11 16:28:41 +0100

    added JS to volume slider

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const playBtn = document.getElementById('play-button'); // why can this not be a
 const volumeIcon = document.getElementById('volume-icon');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
-const currentTime = document.querySelector('.time-elasped');
+const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
@@ -17,7 +17,6 @@ function showPlayIcon() {
 }
 
 function togglePlay() {
-  console.log('click');
   if (video.paused) {
     video.play();
     playBtn.classList.replace('fa-play', 'fa-pause');
@@ -28,9 +27,56 @@ function togglePlay() {
   }
 }
 
+// On video end, show play button icon
+
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+//update progress bar as video play
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// click to seek within the video
+function setProgressBar(e) {
+  const barClickX = (e.offsetX / this.clientWidth) * 100; // where we're clicking on the progress-range divided by the container length. Then multiplied by 100 so we find the percentage of where we click on the progress-range
+  progressBar.style.width = `${barClickX}%`;
+  const newCurrentTime = (video.duration / 100) * barClickX;
+  video.currentTime = `${newCurrentTime}`;
+}
+
 // Volume Controls --------------------------- //
+// volume bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // rounding volume up or down
+  if (volume < 0.1) {
+    volume = 0;
+  }
+  if (volume > 0.9) {
+    volumne = 1;
+  }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+
+  // change icon depending on volume
+  volumeIcon.className = ''; // remove class names
+  if (volume > 0.7) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -39,4 +85,8 @@ function togglePlay() {
 /* Event listeners */
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
 video.addEventListener('ended', showPlayIcon);
+progressRange.addEventListener('click', setProgressBar);
+volumeRange.addEventListener('click', changeVolume);
