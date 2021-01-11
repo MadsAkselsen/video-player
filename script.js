7f304f4d292a4820d2a234c07b6554bff2cb748b
Mads Akselsen <akselsenmads@gmail.com>
2021-01-11 13:42:45 +0100

    added play / pause JS

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-
+const video = document.querySelector('.video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-button'); // why can this not be a querySelector with class?
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elasped');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+}
 
+function togglePlay() {
+  console.log('click');
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+  } else {
+    video.pause();
+    showPlayIcon();
+  }
+}
 
 // Progress Bar ---------------------------------- //
 
-
-
 // Volume Controls --------------------------- //
-
-
 
 // Change Playback Speed -------------------- //
 
-
-
 // Fullscreen ------------------------------- //
 
-
+/* Event listeners */
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('ended', showPlayIcon);
